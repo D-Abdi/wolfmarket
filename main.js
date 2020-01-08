d6af6b3d823f790f1a99128559a5c85bfbe2d250
DanielCMGT <56637726+DanielCMGT@users.noreply.github.com>
2020-01-08 14:02:00 +0100

    JavaScript en Media query's

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 /*  Big button */
 const scrollToBottomBtn = document.querySelector("#scrollToBottomBtn");
+let offsets = document.getElementById('c3').getBoundingClientRect();
+let contactx = offsets.top;
+
+
+
+window.addEventListener("resize", function () {
+    let offsets = document.getElementById('c3').getBoundingClientRect();
+    contactx = offsets.top;
+});
 
 scrollToBottomBtn.addEventListener("click", function () {
     window.scrollTo( {
-        top: 1550,
+        top: contactx,
         left: 0,
         behavior: "smooth"
 
     });
 });
+
